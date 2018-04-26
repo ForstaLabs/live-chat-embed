6effa8d0ea13fb9bd5e40fb81f3e07599b5b1e7a
tyrocoders <zach@tyrocoders.com>
2018-04-25 21:36:13 -0600

    updated $ to jQuery

--- client/app.js
+++ client/app.js
@@ -8,46 +8,46 @@ var flc = {};
 	var isMobileDevice = false;
 
 	function activate() {
-		$('#forsta-chat-container').append('<style>.forsta-chat {position: fixed;bottom: 90;right: 90;z-index: 1000;}</style>');
-		$('#forsta-chat-container').append('<style>.forsta-chat-btn { position: fixed;bottom: 20;right: 20;z-index: 1000;cursor: pointer;outline: none;}</style');
-		$('#forsta-chat-container').append('<style>.rotated-image {-webkit-transform: rotate(360deg);transform: rotate(360deg);transition-duration: .5s;}</style');
+		jQuery('#forsta-chat-container').append('<style>.forsta-chat {position: fixed;bottom: 90;right: 90;z-index: 1000;}</style>');
+		jQuery('#forsta-chat-container').append('<style>.forsta-chat-btn { position: fixed;bottom: 20;right: 20;z-index: 1000;cursor: pointer;outline: none;}</style');
+		jQuery('#forsta-chat-container').append('<style>.rotated-image {-webkit-transform: rotate(360deg);transform: rotate(360deg);transition-duration: .5s;}</style');
 	}
 
 	function initChat(options) {	
 		var src = buildSrc(options);
-		var isMobileDevice = $(window).width() < 992;
+		var isMobileDevice = jQuery(window).width() < 992;
 		if(isMobileDevice) getMobileButton(src, options);
 		else getDesktopButton(src, options);
 	}
 
 	function getMobileButton(src, options) {
-		$('#forsta-chat-container').append('<a class="forsta-chat-btn" id="forsta-chat-mobile" href="'+src+'" target="_blank></a>">');
-		$('#forsta-chat-mobile').append('<img id="chat-open" width="50" src="'+options.openLogo+'">');
+		jQuery('#forsta-chat-container').append('<a class="forsta-chat-btn" id="forsta-chat-mobile" href="'+src+'" target="_blank></a>">');
+		jQuery('#forsta-chat-mobile').append('<img id="chat-open" width="50" src="'+options.openLogo+'">');
 	}
 
 	function getDesktopButton(src, options) {
-		$('#forsta-chat-container').append('<iframe class="forsta-chat" id="forsta-chat" width="400" height="400" src="'+src+'"></iframe>');
-		$('#forsta-chat-container').append('<a class="forsta-chat-btn" id="forsta-chat-desktop"></a>');
-		$('#forsta-chat-desktop').append('<img id="chat-open" width="60" src="'+options.openLogo+'">');
-		$('#forsta-chat-desktop').append('<img id="chat-close" width="60" src="'+options.closeLogo+'">');
-		$('#forsta-chat').hide();
-		$('#chat-close').hide();
+		jQuery('#forsta-chat-container').append('<iframe class="forsta-chat" id="forsta-chat" width="400" height="400" src="'+src+'"></iframe>');
+		jQuery('#forsta-chat-container').append('<a class="forsta-chat-btn" id="forsta-chat-desktop"></a>');
+		jQuery('#forsta-chat-desktop').append('<img id="chat-open" width="60" src="'+options.openLogo+'">');
+		jQuery('#forsta-chat-desktop').append('<img id="chat-close" width="60" src="'+options.closeLogo+'">');
+		jQuery('#forsta-chat').hide();
+		jQuery('#chat-close').hide();
 		addDesktopBtnListener();
 	}
 
 	function addDesktopBtnListener(options) {
-		$('#forsta-chat-desktop').click(function() {
-			$('#forsta-chat-desktop').addClass("rotated-image");
+		jQuery('#forsta-chat-desktop').click(function() {
+			jQuery('#forsta-chat-desktop').addClass("rotated-image");
 			if(!chatOpen) {
-				$('#chat-open').hide();
-				$('#chat-close').show();
+				jQuery('#chat-open').hide();
+				jQuery('#chat-close').show();
 			}
 			else {
-				$('#chat-open').show();
-				$('#chat-close').hide();
+				jQuery('#chat-open').show();
+				jQuery('#chat-close').hide();
 			}
-			$('#forsta-chat').slideToggle( "slow", function() {
-				$('#forsta-chat-desktop').removeClass("rotated-image");
+			jQuery('#forsta-chat').slideToggle( "slow", function() {
+				jQuery('#forsta-chat-desktop').removeClass("rotated-image");
 				chatOpen = !chatOpen;			
 			});
 		});
