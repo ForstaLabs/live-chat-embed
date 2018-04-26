fddb551ce4a0a3003ed508f9aeca82447c23d8ce
tyrocoders <zach@tyrocoders.com>
2018-04-25 20:47:07 -0600

    got it working

--- client/app.js
+++ client/app.js
@@ -4,55 +4,28 @@ var isMobileDevice = false;
 activate();
 
 function activate() {
-	$('#forsta-chat-container').append(`
-		<style>
-		.forsta-chat {    
-			position: fixed;
-			bottom: 90;
-			right: 90;
-			z-index: 1000;
-		}
-		.forsta-chat-btn {    
-			position: fixed;
-			bottom: 20;
-			right: 20;
-			z-index: 1000;
-			cursor: pointer;
-			outline: none;
-		}
-		.rotated-image {
-			-webkit-transform: rotate(360deg);
-			transform: rotate(360deg);
-			transition-duration: .5s;
-		}
-		</style>	
-	`);
+	$('#forsta-chat-container').append('<style>.forsta-chat {position: fixed;bottom: 90;right: 90;z-index: 1000;}</style>');
+	$('#forsta-chat-container').append('<style>.forsta-chat-btn { position: fixed;bottom: 20;right: 20;z-index: 1000;cursor: pointer;outline: none;}</style');
+	$('#forsta-chat-container').append('<style>.rotated-image {-webkit-transform: rotate(360deg);transform: rotate(360deg);transition-duration: .5s;}</style');
 }
 
-function setChat(options) {
+function initChat(options) {	
+	var src = buildSrc(options);
 	var isMobileDevice = $(window).width() < 992;
-	if(isMobileDevice) getMobileButton(options);
-	else getDesktopButton(options);
+	if(isMobileDevice) getMobileButton(src, options);
+	else getDesktopButton(src, options);
 }
 
-function getMobileButton(options) {
-	$('#forsta-chat-container').append(`
-		<a class="forsta-chat-btn" id="forsta-chat-mobile" href="${buildSrc(options)}">
-			<img id="chat-open" width="50" src="../client/chat-open.png">
-		</a>	
-	`);
+function getMobileButton(src, options) {
+	$('#forsta-chat-container').append('<a class="forsta-chat-btn" id="forsta-chat-mobile" href="'+src+'" target="_blank></a>">');
+	$('#forsta-chat-mobile').append('<img id="chat-open" width="50" src="'+options.openLogo+'">');
 }
 
-function getDesktopButton(options) {
-	$('#forsta-chat-container').append(`		
-		<iframe class="forsta-chat" id="forsta-chat" width="400" height="400" href="${buildSrc(options)}"></iframe>
-
-
-		<a class="forsta-chat-btn" id="forsta-chat-desktop">	
-			<img id="chat-open" width="50" src="../client/chat-open.png">
-			<img id="chat-close" width="50" src="../client/chat-close.png">
-		</a>
-	`);	
+function getDesktopButton(src, options) {
+	$('#forsta-chat-container').append('<iframe class="forsta-chat" id="forsta-chat" width="400" height="400" src="'+src+'"></iframe>');
+	$('#forsta-chat-container').append('<a class="forsta-chat-btn" id="forsta-chat-desktop"></a>');
+	$('#forsta-chat-desktop').append('<img id="chat-open" width="60" src="'+options.openLogo+'">');
+	$('#forsta-chat-desktop').append('<img id="chat-close" width="60" src="'+options.closeLogo+'">');
 	$('#forsta-chat').hide();
 	$('#chat-close').hide();
 	addDesktopBtnListener();
