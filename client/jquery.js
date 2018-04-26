fb447bd59bb56cbadaa522b6add875c28f1acb66
tyrocoders <zach@tyrocoders.com>
2018-04-25 21:21:38 -0600

    changed jquery cdn to google for https

--- client/jquery.js
+++ client/jquery.js
@@ -1,5 +1,5 @@
 window.jQuery || (function() {
 	var scriptElem = document.createElement('script');
-	scriptElem.setAttribute('src','http://code.jquery.com/jquery-3.3.1.min.js');
+	scriptElem.setAttribute('src','https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js');
 	document.body.appendChild(scriptElem);
 })();
